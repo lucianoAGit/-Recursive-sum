39f9230f37cccad3cecc36ba05fff6b907b8ecae
lucianoAGit <lucianogabriel2468@usp.br>
2019-09-26 13:31:26 -0300

    Soma recursiva

---
+++ somarec.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int somarec(int n[10], int k){
+	int s = 0;
+	
+	if(k == -1)
+		return s;
+	else
+		s = s + n[k];
+
+return s + somarec(n,k-1);
+}
+
+
+void main(){
+	int n[10],k,i;
+	
+	printf("Entre com o tamanho da sequencia que deseja somar: ");
+	scanf("%d",&k);
+	
+	for (i=0;i<k;i++){
+		printf("Entre com o elemento: ");
+		scanf("%d", &n[i]);
+	}
+	printf("Essa eh a sua soma: %d ", somarec(n,k-1));
+}
